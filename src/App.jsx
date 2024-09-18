49b622f6a25ec49c72b2e0ddfe0b067f89d6f8d6
Phu Thinh <truongdophuthinh@gmail.com>
2024-09-18 20:03:52 +0700

    frontend todo-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MyComponent from './components/learn/MyComponent'
-import { FirstComponent, SecondComponent } from './components/learn/SecondComponent'
+import "./components/todo/todo.css"
+import TodoData from "./components/todo/TodoData"
+import TodoNew from "./components/todo/TodoNew"
+import reactLogo from "./assets/react.svg"
 
 const App = () => {
-
-  const myFunction = () => {
-    console.log("hello world")
-  }
-
-  myFunction()
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello World</h1>
-      <MyComponent/>
-      <FirstComponent/>
-      <SecondComponent/>
-    </>
+   <div className="todo-container">
+    <div className="todo-title">Todo List</div>
+    <TodoNew/>
+    <div className="todo-image">
+      <img src={reactLogo} alt="" />
+    </div>
+    <TodoData/>
+   </div>
   )
 }
 
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,10 @@
+const TodoNew = () => {
+    return (
+        <div className="todo-create">
+            <input type="text" placeholder="Enter task"/>
+            <button>Add</button>
+        </div>
+    )
+}
+
+export default TodoNew
