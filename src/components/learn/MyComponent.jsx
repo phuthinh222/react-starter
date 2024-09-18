9843c01f3d8270449409d7fe6bfef0502d855f45
Phu Thinh <truongdophuthinh@gmail.com>
2024-09-18 19:22:42 +0700

    data types react

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -5,10 +5,24 @@
 import './style.css';
 
 const MyComponent = () => {
+
+  const name = "thinh" // string
+  const age = 25 // number
+  const check = true // boolean
+  const number = [1,2,3]
+  const info = {
+    name: "Thinh",
+    age: 22
+  }
+
     return (
       <>
-        <div className='child'>My name is Thinh</div>
-        <div style={ {backgroundColor : "blue"} }>Age: 22</div>
+        <div className='child'>My name is {name}</div>
+        <div style={ {backgroundColor : "blue"} }>Age: {age}</div>
+        <div>{check}</div>
+        <div>{number}</div>
+        <div>Information {JSON.stringify(info)}</div>
+        <div>{console.log("Hello 123")}</div>
       </>
     )
 }
